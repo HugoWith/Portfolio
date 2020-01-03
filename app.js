25b8d8aa183031276afe4877ae39e20797285c77
Hugo Valla <Hugo_valla@hotmail.fr>
2020-01-03 16:54:24 +0100

    V1

---
+++ app.js
@@ -0,0 +1,26 @@
+new fullpage("#fullpage", {
+    //options here
+    autoScrolling: true,
+    navigation: true,
+    onLeave: (origin, destination, direction) => {
+      const section = destination.item;
+      const title = section.querySelector("h1");
+      const tl = new TimelineMax({ delay: 0.5 });
+      tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "-40", opacity: 1 });
+
+        if (destination.index === 1) {
+            const hugoimg = document.querySelectorAll(".hugoimg");
+            const description = document.querySelector(".description");
+            tl.fromTo(hugoimg, 2, { opacity: 0, x: "-80%" }, { opacity: 1, x: "30%" })
+              .fromTo(
+                description,
+                0.8,
+                { opacity: 0, y: "50" },
+                { y: "0", opacity: 1 }
+              )
+              .fromTo(hugoimg[0], 1, { opacity: 0.3 }, { opacity: 1 })
+              .fromTo(hugoimg[1], 2, { opacity: 0 }, { opacity: 1 })
+              .fromTo(hugoimg[2], 3, { opacity: 0 }, { opacity: 1 });
+          }
+    }
+});
